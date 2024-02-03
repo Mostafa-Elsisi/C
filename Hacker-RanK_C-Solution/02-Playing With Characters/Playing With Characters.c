00379dcb1daa3dc18ab78daa592b7e4e15725980
Mostafa-Elsisi <elsisimostafa57@gmail.com>
2024-02-03 10:56:30 +0200

    Edit on Playing With Character

--- Hacker-RanK_C-Solution/02-Playing With Characters/Playing With Characters.c
+++ Hacker-RanK_C-Solution/02-Playing With Characters/Playing With Characters.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
 
-int main() 
+int main()
 {
     char ch;
-    char s[46]; //an array of 46 because the longest word in english is 45 char. long 
-    char sen[100]; 
-    
+    char s[46]; 
+    char sen[100];
+
     scanf("%c", &ch );
     scanf("%s \n", &s );
-    scanf("%[^\n]%*c", &sen );
-    
+    scanf("%[^\n]", sen );
+
     printf("%c \n", ch);
     printf("%s \n", s);
-    printf("%s", sen);        
-      
+    printf("%s", sen);
+
     return 0;
 }
